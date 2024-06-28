226411ac14aba6ac6324e502f5030f967039fb06
OrryFrasetyo <orrytio12345@gmail.com>
2024-06-28 22:30:28 +0700

    setup kategori

--- app/src/main/java/com/orryfrasetyo/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/orryfrasetyo/jetcoffee/MainActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.orryfrasetyo.jetcoffee.model.dummyCategory
+import com.orryfrasetyo.jetcoffee.ui.components.CategoryItem
 import com.orryfrasetyo.jetcoffee.ui.components.Search
+import com.orryfrasetyo.jetcoffee.ui.components.SectionText
 import com.orryfrasetyo.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +38,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
-    Column {
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -50,6 +61,21 @@ fun Banner(
     }
 }
 
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun JetCoffeeAppPreview() {
@@ -57,3 +83,22 @@ fun JetCoffeeAppPreview() {
         JetCoffeeApp()
     }
 }
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
