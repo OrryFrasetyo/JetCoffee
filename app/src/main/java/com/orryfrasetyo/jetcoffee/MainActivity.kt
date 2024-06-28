1d4b4ded308f27e444b67a546d8754c0c2647860
OrryFrasetyo <orrytio12345@gmail.com>
2024-06-28 23:18:41 +0700

    setup menu item

--- app/src/main/java/com/orryfrasetyo/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/orryfrasetyo/jetcoffee/MainActivity.kt
@@ -9,9 +9,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -20,8 +21,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.orryfrasetyo.jetcoffee.model.Menu
+import com.orryfrasetyo.jetcoffee.model.dummyBestSellerMenu
 import com.orryfrasetyo.jetcoffee.model.dummyCategory
+import com.orryfrasetyo.jetcoffee.model.dummyMenu
 import com.orryfrasetyo.jetcoffee.ui.components.CategoryItem
+import com.orryfrasetyo.jetcoffee.ui.components.MenuItem
 import com.orryfrasetyo.jetcoffee.ui.components.Search
 import com.orryfrasetyo.jetcoffee.ui.components.SectionText
 import com.orryfrasetyo.jetcoffee.ui.theme.JetCoffeeTheme
@@ -38,11 +43,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun JetCoffeeApp() {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -72,6 +81,22 @@ fun CategoryRow(
     ) {
         items(dummyCategory, key = { it.textCategory }) { category ->
             CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
         }
     }
 }
